b1bec36fd5a10ef302a665b438a1ece32e705963
Paydhi <paydhi@gmail.com>
2023-04-24 22:48:59 +0600

    added working posts/api

--- daemmit-project/posts/views.py
+++ daemmit-project/posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+from posts.models import Post
+from posts.serializers import PostSerializer
 
 # Create your views here.
+
+class PostList(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
