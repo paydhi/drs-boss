c28c3b6d9f010f19d2df97e193fd5fa3a868e7d4
Paydhi <paydhi@gmail.com>
2023-04-24 23:39:20 +0600

    added authentication for POST requests

--- daemmit-project/posts/views.py
+++ daemmit-project/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics
+
+from rest_framework import generics, permissions
 from posts.models import Post
 from posts.serializers import PostSerializer
 
@@ -8,3 +9,6 @@ from posts.serializers import PostSerializer
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    def perform_create(self, serializer):
+        serializer.save(poster=self.request.user)
