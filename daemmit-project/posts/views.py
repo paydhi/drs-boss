54ef5a42d148bddb5ccb227600c176eb617c02a1
Paydhi <paydhi@gmail.com>
2023-04-25 11:08:08 +0600

    added user validation for delete post

--- daemmit-project/posts/views.py
+++ daemmit-project/posts/views.py
@@ -35,9 +35,16 @@ class VoteCreate(generics.CreateAPIView, mixins.DestroyModelMixin):
             self.get_queryset.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
         else:
-            raise ValidationError('You never voted for this vote')
+            raise ValidationError('You never voted for this vote.')
 
 class PostRetrieveDestroy(generics.RetrieveDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self, request, *args, **kwargs):
+        post = Post.objects.filter(pk=kwargs['pk'], poster=self.request.user)
+        if post.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError('This is not your post.')
